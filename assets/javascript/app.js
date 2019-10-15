3678ca0edc83ad390040b5f2ea15bcd6de52d203
olive <daniellek@mobilefuse.com>
2019-10-15 18:58:18 -0400

    having an issue with pushing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,21 @@
 
-  
+  window.onload = function() {
+    $("#start").on("click", start);
+  };
+//click to start the timer
+  function start() {
 
-  
     let counter = 60;
     let interval = setInterval(function() {
         counter--;
         // Display 'counter' wherever you want to display it.
         if (counter <= 0) {
              clearInterval(interval);
-            $('#timer').html("<h3>Count down complete</h3>");  
+            $('#timer').html("<h3>Times Up! Lets see your results...</h3>");  
             return;
         }else{
           $('#time').text(counter);
           console.log("Timer --> " + counter);
         }
     }, 1000);
+  }
